f65907acc70a8915fb8c25e774b8cb26bca290b2
rkscoggins3 <91280262+rkscoggins3@users.noreply.github.com>
2021-09-23 13:45:30 -0400

    Update Server.R

--- Server.R
+++ Server.R
@@ -46,10 +46,10 @@ fillmap2<-function(map, figtitle, y , leg.loc="beside", y.scl=NULL,
   }
 }
 
-data <- read.csv('Data4Shiny.csv')
+data <- read.csv('Data\\Data4Shiny.csv')
 head(data)
 
-NHtracts=readOGR("NHtracts.shp")
+NHtracts=readOGR("Data\\NHtracts.shp")
 
 
 #simply plot the arrest data
